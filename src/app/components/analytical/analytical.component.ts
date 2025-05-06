beebd52837581c5d3411dacab3cf406dc690eb8c
arafattech <arahman@hybri.tech>
2025-05-06 14:26:10 +0600

    Project Run Done

---
+++ src/app/components/analytical/analytical.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytical',
+  standalone: true,
+  imports: [],
+  templateUrl: './analytical.component.html',
+  styleUrl: './analytical.component.css'
+})
+export class AnalyticalComponent {
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+}
